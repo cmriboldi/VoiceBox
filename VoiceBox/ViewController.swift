839e37b4e0474f0d7261d1d1a84f9ee717cf1ef2
Christian Riboldi <cmriboldi@gmail.com>
2018-01-16 09:17:42 -0700

    Initial Commit

---
+++ VoiceBox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VoiceBox
+//
+//  Created by Christian Riboldi on 1/16/18.
+//  Copyright © 2018 Christian Riboldi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

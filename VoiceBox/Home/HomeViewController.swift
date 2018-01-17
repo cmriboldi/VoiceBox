4ad4db9c6cb652e8a4791f16c2cea28f5ea2f25c
Christian Riboldi <cmriboldi@gmail.com>
2018-01-17 08:59:59 -0700

    Adds a basic UI for the application and organizes all of the project files.

--- VoiceBox/Home/HomeViewController.swift
+++ VoiceBox/Home/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  VoiceBox
 //
 //  Created by Christian Riboldi on 1/16/18.
@@ -8,18 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 

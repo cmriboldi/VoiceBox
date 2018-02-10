da3ea0ba92db9a94e23520b3e6cf393d37e1fa63
Andrew Hale <anderillohale@gmail.com>
2018-02-09 22:55:04 -0700

    Added sentence builder thing.

--- VoiceBox/Home/HomeViewController.swift
+++ VoiceBox/Home/HomeViewController.swift
@@ -13,46 +13,48 @@ class HomeViewController: UIViewController, UITextFieldDelegate {
     var prevWord: Word = Word(value: "", imageName: "")
     
     @IBOutlet weak var inputWord: UITextField!
+    var currentWord: String = ""
     @IBOutlet weak var mainWord: UIButton!
+    var likelyNextWords: [String] = ["about", "after", "again", "ah", "all"]
+    @IBOutlet var wordButtons: [UIButton]!
+    
     @IBAction func train(_ sender: UIButton) {
         let trainer = Trainer()
         trainer.train(name: "train", extension: "txt")
     }
-    @IBOutlet weak var wordList: WordList!
     
-//    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
-//
-//        if viewController == self {return false}
-//
-//        let fromView = self.view
-//        let toView = viewController.view
-//
-//        UIView.transition(from: fromView!, to: toView!, duration: 0.3, options: [.transitionCrossDissolve], completion: nil)
-//
-//        return true
-//    }
+    //TODO
+    @IBAction func wordPressed(_ sender: Any) {
+//        print("pressed")
+        var newWord = (sender as! UIButton).currentTitle
+        var newScreen = HomeViewController.makeFromStoryboard()
+        var probableWords = self.doMachineLearning(textWord: newWord!, numWords: 5)
+        
+        newScreen.currentWord = newWord!
+        
+        newScreen.likelyNextWords = []
+        for word in probableWords {
+            newScreen.likelyNextWords.append(word)
+        }
+        
+        self.present(newScreen, animated: true, completion: nil)
+    }
+    
+    @IBOutlet weak var wordList: WordList!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         inputWord.delegate = self
         
-//        self.tabBarController.setSwipeAnimation(type: SwipeAnimationType.sideBySide)
-        
-//        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(swiped))
-//        swipeRight.direction = UISwipeGestureRecognizerDirection.right
-//        self.view.addGestureRecognizer(swipeRight)
-//
-//        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(swiped))
-//        swipeLeft.direction = UISwipeGestureRecognizerDirection.left
-//        self.view.addGestureRecognizer(swipeLeft)
-
-        mainWord?.layer.cornerRadius = 0.5 * mainWord.bounds.size.width
-        mainWord?.layer.borderColor = UIColor.black.cgColor
-        mainWord?.layer.borderWidth = 2.0
-        
         let word1 = VocabDatabase.shared.wordForId(1)
         print("Done getting word:\(word1)")
+        
+        self.mainWord.setTitle(self.currentWord, for: .normal)
+        
+        for i in 0..<min(5, self.likelyNextWords.count) {
+            self.wordButtons[i].setTitle(self.likelyNextWords[i], for: .normal)
+        }
     }
     
     //MARK: UITextFieldDelegate
@@ -66,8 +68,6 @@ class HomeViewController: UIViewController, UITextFieldDelegate {
         var probableWords = [String]()
         
         let ngram = NGram()
-//        ngram.train(textFilePath: "/Users/andrewhale/Documents/CS498R/VoiceBox/VoiceBox/Shared/Data/train.txt", n: 3)
-//        probableWords = ngram.nextWords(textPrevWord: word, textWord: word, numWords: numWords)
         let word = VocabDatabase.shared.getWord(word: textWord)
         probableWords = ngram.nextWords(prevWord: self.prevWord, word: word, numWords: numWords)
         self.textPrevWord = textWord
@@ -84,21 +84,10 @@ class HomeViewController: UIViewController, UITextFieldDelegate {
         wordList.setupWords()
     }
     
-//    @objc func swiped(_ gesture: UISwipeGestureRecognizer) {
-//        if gesture.direction == .left {
-//            if (self.tabBarController?.selectedIndex)! < 3 { // set your total tabs here
-////                let fromView = self.tabBarController?.selectedViewController!.view
-//                self.tabBarController?.selectedIndex += 1
-//
-////                let toView = self.tabBarController?.selectedViewController!.view
-////
-////                UIView.transition(from: fromView!, to: toView!, duration: 0.3, options: [.transitionCrossDissolve], completion: nil)
-//            }
-//        }
-//        else if gesture.direction == .right {
-//            if (self.tabBarController?.selectedIndex)! > 0 {
-//                self.tabBarController?.selectedIndex -= 1
-//            }
-//        }
-//    }
+    static func makeFromStoryboard() -> HomeViewController {
+        guard let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "HomeViewController") as? HomeViewController else {
+            return HomeViewController()
+        }
+        return viewController
+    }
 }
